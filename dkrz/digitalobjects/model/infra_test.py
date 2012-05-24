0eb7798e9c204afdfd282445ec3c7ae136d3b8d2
Tobias Weigel <weigel@dkrz.de>
2012-05-24 17:25:46 +0200

    testing for references

--- dkrz/digitalobjects/model/infra_test.py
+++ dkrz/digitalobjects/model/infra_test.py
@@ -26,12 +26,28 @@ class TestDOInfrastructure(unittest.TestCase):
     def tearDown(self):
         pass
     
+    def __check_dobj3_references(self, dobj3, dobj1, dobj2):
+        i = 0
+        iter3 = dobj3.iter_reference_keys()
+        for k in iter3:
+            assert k == "successor"
+            i += 1
+        assert i == 1
+        assert dobj3.get_references("notexisting") == []
+        succ = dobj3.get_references("successor")
+        assert len(succ) == 2
+        assert succ[0].identifier == dobj1.identifier
+        assert succ[1].identifier == dobj2.identifier
+
+    
     def test_do_values(self):
         # values
         pid = self.prefix+"test_do_values"
         resloc = "http://www.example.com/1"
         restype = "MY_TEST_TYPE"
         annot1 = "test annotation 1"
+        pid2 = self.prefix+"test_do_values_2"
+        pid3 = self.prefix+"test_do_values_3"
         # create DO
         dobj = self.do_infra.create_do(pid)
         self.created_pids.append(pid)
@@ -50,10 +66,27 @@ class TestDOInfrastructure(unittest.TestCase):
         assert dobj2.resource_type == restype
         assert dobj2.get_annotation("annot1") == annot1
         assert dobj2.get_annotation("nonexisting") == None
+        # create a second DO
+        dobj2 = self.do_infra.create_do(pid2)
+        self.created_pids.append(pid2)
+        assert dobj2 != None
+        # create a third DO
+        dobj3 = self.do_infra.create_do(pid3)
+        self.created_pids.append(pid3)
+        assert dobj3 != None
+        dobj3.add_do_reference("successor", dobj)
+        dobj3.add_do_reference("successor", dobj2)
+        dobj2.add_do_reference("predecessor", dobj3)
+        dobj.add_do_reference("predecessor", dobj3)
+        # check references
+        self.__check_dobj3_references(dobj3, dobj, dobj2)
+        # retrieve dobj3 from infra and check references
+        dobj3 = self.do_infra.lookup_pid(pid3)
+        self.__check_dobj3_references(dobj3, dobj, dobj2)
         # delete and check for removal
         self.do_infra.delete_do(pid)
-        dobj2 = self.do_infra.lookup_pid(pid)
-        assert dobj2 == None
+        dobj = self.do_infra.lookup_pid(pid)
+        assert dobj == None
         
     def test_infra_operations(self):
         dobj = self.do_infra.lookup_pid(self.prefix+"does-not-exist")
